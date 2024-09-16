a0d13fba504d5bbfaba6424e8b69a9beead38c27
Igor Motyka <igor_motyka@mail.com>
2024-09-16 17:32:21 +0200

    Testing out iterator idea

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,59 @@ use std::env;
 use std::io;
 use std::process;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+enum Symbol {
+    Number,
+    Text(String),
+}
+
+struct Pattern {
+    source: Vec<char>,
+    index: usize,
+    length: usize,
+}
+
+impl Pattern {
+    pub fn new(source: &str) -> Self {
+        Self {
+            source: source.chars().collect(),
+            index: 0,
+            length: 0,
+        }
     }
+}
+
+impl Iterator for Pattern {
+    type Item = Symbol;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buffer: Vec<char> = Vec::with_capacity(16);
+
+        for i in self.index..self.source.len() {
+            let char = self.source[i];
+            self.index += 1;
+
+            let symbol = match buffer.as_slice() {
+                ['\\', 'd'] => Some(Symbol::Number),
+                _ => None,
+            };
+
+            if symbol.is_some() {
+                return symbol;
+            }
+
+            buffer.push(char);
+        }
+
+        None
+    }
+}
+
+fn match_pattern(input_line: &str, raw_pattern: &str) -> bool {
+    let mut pattern = Pattern::new(raw_pattern);
+
+    for symbol in pattern {}
+
+    false
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
@@ -25,7 +72,6 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
     if match_pattern(&input_line, &pattern) {
         process::exit(0)
     } else {
