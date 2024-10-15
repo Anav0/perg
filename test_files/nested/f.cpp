a05088a3f9f26a1d702fa473fdb2ae8163e52dbc
Igor Motyka <igor_motyka@mail.com>
2024-10-15 19:10:39 +0200

    More test files

---
+++ test_files/nested/f.cpp
@@ -0,0 +1,8 @@
+#include "f.h"
+
+void foo1() {}
+void foo2() {}
+void bar3() {}
+void bar4() {}
+void car5() {}
+void car6() {}
--- test_files/nested/f.h
+++ test_files/nested/f.h
@@ -1,6 +1,6 @@
-void foo1() {}
-void foo2() {}
-void bar3() {}
-void bar4() {}
-void car5() {}
-void car6() {}
+void foo1();
+void foo2();
+void bar3();
+void bar4();
+void car5();
+void car6();
