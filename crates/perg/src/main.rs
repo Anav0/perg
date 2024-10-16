abc5da6485a6cc3d7de4480aed878f3f2af85053
Igor Motyka <igor_motyka@mail.com>
2024-10-17 00:06:10 +0200

    Change multithreaded code to use futures crate with ThreadPool

--- crates/perg/src/main.rs
+++ crates/perg/src/main.rs
@@ -1,14 +1,12 @@
 use bolg::glob;
-use std::thread;
 use clap::{command, Parser};
+use futures::executor::{block_on, ThreadPool};
+use futures::future::join_all;
+use futures::task::SpawnExt;
 use lazy_static::lazy_static;
 use nfa::{FileMatch, NfaOptions, NFA};
 use re::regex_to_nfa;
-use std::{
-    collections::HashSet,
-    fs,
-    path::{Path, PathBuf},
-};
+use std::{collections::HashSet, fs, path::PathBuf};
 
 mod misc;
 mod nfa;
@@ -54,19 +52,45 @@ struct Args {
     path: String,
 }
 
+async fn find_matches_in_files(chunk: Vec<PathBuf>, args: Args, options: NfaOptions) -> Vec<FileMatch> {
+    let nfa = regex_to_nfa(&args.pattern, &options);
+    let mut output: Vec<FileMatch> = vec![];
+    for file_path in chunk {
+        if let Ok(m) = fs::metadata(&file_path) {
+            if m.is_dir() {
+                continue;
+            }
+            let input = fs::read_to_string(&file_path).expect(&format!(
+                "Failed to read input file: '{}'",
+                file_path.to_str().unwrap()
+            ));
+            let matches = nfa.find_matches(&input);
+            let file_match = FileMatch {
+                file_path: Some(PathBuf::from(file_path)),
+                matches,
+            };
+            output.push(file_match);
+        }
+    }
+    output
+}
+
 fn main() {
+    let executor = ThreadPool::new().unwrap();
     let args = Args::parse();
 
     let path = PathBuf::from(&args.path);
 
     let options = NfaOptions::from(&args);
 
-    let number_of_available_threads = std::thread::available_parallelism().expect("Cannot determin number of CPU cores");
-
+    let number_of_available_threads =
+        std::thread::available_parallelism().expect("Cannot determin number of CPU cores");
 
     let mut files = vec![];
     for pattern in &args.glob {
-        let mut matched_files = glob(pattern, &path).expect("Cannot perform glob search").collect::<Vec<_>>();
+        let mut matched_files = glob(pattern, &path)
+            .expect("Cannot perform glob search")
+            .collect::<Vec<_>>();
         files.append(&mut matched_files);
     }
 
@@ -76,53 +100,36 @@ fn main() {
         chunk_size = files.len();
     }
 
-    debug_println!("Threads: {}, Files matched: {}, Chunk size: {}", number_of_available_threads, files.len(), chunk_size);
+    if chunk_size == 0 {
+        return;
+    }
+
+    debug_println!(
+        "Threads: {}, Files matched: {}, Chunk size: {}",
+        number_of_available_threads,
+        files.len(),
+        chunk_size
+    );
 
     let mut handles = vec![];
     for chunk in files.chunks(chunk_size) {
-        let args_clone = args.clone();
         let chunk = chunk.to_vec();
-        let handle = thread::spawn(move || {
-            let options = NfaOptions::from(&args_clone);
-            let nfa = regex_to_nfa(&args_clone.pattern, &options);
-            let mut output: Vec<FileMatch> = vec![];
-            for file_path in chunk {
-              if let Ok(m) = fs::metadata(&file_path) {
-                if m.is_dir() {
-                    return Err("Glob returned directory not a file!");
-                }
-                let input = fs::read_to_string(&file_path).expect(&format!(
-                    "Failed to read input file: '{}'",
-                    file_path.to_str().unwrap()
-                ));
-                let matches = nfa.find_matches(&input);
-                let file_match = FileMatch {
-                    file_path: Some(PathBuf::from(file_path)),
-                    matches,
-                };
-                output.push(file_match);
-            }
-        }
-        Ok(output)
-        });
+        let fut = find_matches_in_files(chunk, args.clone(), options.clone());
+        let handle = executor.spawn_with_handle(fut).expect("Failed to spawn thread");
         handles.push(handle);
     }
 
-    for h in handles {
-        match h.join().expect("Failed to await thread") {
-            Ok(matches) => {
-                if args.count {
-                    for m in matches {
-                        m.print_count();
-                    }
-                }else {
-                    for m in matches {
-                        m.print_matches(&options);
-                    }
-                }
-            },
-            Err(_) => eprintln!("Failed to find matches!"),
+    let results = block_on(join_all(handles));
+
+    for matches in results {
+        if args.count {
+            for m in matches {
+                m.print_count();
+            }
+        } else {
+            for m in matches {
+                m.print_matches(&options);
+            }
         }
     }
-
 }
